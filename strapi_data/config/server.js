dc7b25ac3fbfe631332ce6c077603c19e8df3edc
extinctCoder <extinctCoder@outlook.com>
2022-03-14 02:26:06 +0600

    Update server.js

--- strapi_data/config/server.js
+++ strapi_data/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  // host: env('HOST', '0.0.0.0'),
-  url: env("WEBSITE_URL", "https://docker-test-007.herokuapp.com/"),
+  host: env("HOST", "0.0.0.0"),
+  //url: env("WEBSITE_URL", "https://docker-test-007.herokuapp.com/"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
